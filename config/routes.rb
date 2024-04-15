be5e5d479df5361a46b210116a138ac48c2dfec0
ibraheemnadeem7 <157302504+ibraheemnadeem7@users.noreply.github.com>
2024-04-15 09:52:26 -0400

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
 
   resources :articles do
     resources :comments
-
+  end
+  
   get "/articles", to: "articles#index"
   get "/articles/:id", to: "articles#show"
   
