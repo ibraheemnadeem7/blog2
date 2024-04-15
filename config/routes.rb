131f19d7a249441b01b5c90091f9ddc8cb58f8b3
ibraheemnadeem7 <157302504+ibraheemnadeem7@users.noreply.github.com>
2024-04-14 22:12:20 -0400

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
     root "articles#index"
 
   get "/articles", to: "articles#index"
-
-
+  get "/articles/:id", to: "articles#show"
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
