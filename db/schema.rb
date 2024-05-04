351a78bf96f690f4f8359c51d98d0004a8ca0d62
ibraheem <nadeem2@kenyon.edu>
2024-05-04 02:59:25 +0000

    lol

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_28_163527) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_04_023739) do
   create_table "articles", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "status", default: "public"
+  end
+
+  create_table "buckets", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "comments", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
@@ -29,5 +37,16 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_28_163527) do
     t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
+  create_table "tasks", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "status"
+    t.bigint "bucket_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["bucket_id"], name: "index_tasks_on_bucket_id"
+  end
+
   add_foreign_key "comments", "articles"
+  add_foreign_key "tasks", "buckets"
 end
