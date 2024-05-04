cbb282010436d659a1ffe7c88b958ca043711cf9
ibraheem <nadeem2@kenyon.edu>
2024-05-04 02:38:14 +0000

    Your descriptive commit message here

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :tasks
+  resources :buckets
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "articles#index"
 
